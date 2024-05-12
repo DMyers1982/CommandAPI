460d8a3bee06e513f33a3a3fb0e0072cdea53778
Dannie Myers <dmyers19@woh.rr.com>
2024-05-12 17:25:53 -0400

    Added code for entity framework

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -5,37 +5,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers
 {
- [Route("api/[controller]")]
- [ApiController]
- public class CommandsController : ControllerBase
- {
-    //Add the following code to our class
-    private readonly ICommandAPIRepo _repository;
-    public CommandsController(ICommandAPIRepo repository)
-    {
-    _repository = repository;
-    }
+   [Route("api/[controller]")]
+   [ApiController]
+   public class CommandsController : ControllerBase
+   {
+      //Add the following code to our class
+      private readonly ICommandAPIRepo _repository;
+      public CommandsController(ICommandAPIRepo repository)
+      {
+      _repository = repository;
+      }
 
-    //Add the following code
- [HttpGet]
- public ActionResult<IEnumerable<Command>> GetAllCommands()
- {
- var commandItems = _repository.GetAllCommands();
- return Ok(commandItems);
- }
+      //Add the following code
+      [HttpGet]
+      public ActionResult<IEnumerable<Command>> GetAllCommands()
+      {
+         var commandItems = _repository.GetAllCommands();
+         return Ok(commandItems);
+      }
 
- //Add the following code for our second ActionResult
-[HttpGet("{id}")]
-public ActionResult<Command> GetCommandById(int id)
-{
- var commandItem = _repository.GetCommandById(id);
- if (commandItem == null)
- {
- return NotFound();
- }
- return Ok(commandItem);
-}
-
+      //Add the following code for our second ActionResult
+      [HttpGet("{id}")]
+      public ActionResult<Command> GetCommandById(int id)
+      {
+         var commandItem = _repository.GetCommandById(id);
+         if (commandItem == null)
+         {
+         return NotFound();
+         }
+         return Ok(commandItem);
+      }
     
-}
+   }
 }
