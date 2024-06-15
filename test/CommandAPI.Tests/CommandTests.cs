cfd446cf3ca4214757c4771204fc97a294dbd103
Dannie Myers <dmyers19@woh.rr.com>
2024-06-15 19:52:37 -0400

    Prior to removing /* AutoMapper*/

--- test/CommandAPI.Tests/CommandTests.cs
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -1,36 +1,59 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using Moq;
+using AutoMapper;
 using CommandAPI.Models;
+using CommandAPI.Data;
+
 namespace CommandAPI.Tests
 {
-    public class CommandTests
+    public class CommandTests : IDisposable
     {
+        Command testCommand;
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+            HowTo = "Do something",
+            Platform = "Some platform",
+            CommandLine = "Some commandline"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
         [Fact]
         public void CanChangeHowTo()
         {
             //Arrange
-            var testCommand = new Command
-            {
-                HowTo = "Do something awesome",
-                Platform = "xUnit",
-                CommandLine = "dotnet test"
-            };
+            //var testCommand = new Command
+            //{
+            //    HowTo = "Do something awesome",
+            //    Platform = "xUnit",
+            //    CommandLine = "dotnet test"
+            //};
+
             //Act
-            testCommand.HowTo = "Do something awesome";
+            testCommand.HowTo = "Execute Unit Tests";
             //Assert
-            Assert.Equal("Do something awesome", testCommand.HowTo);
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
         }
 
         [Fact]
         public void CanChangePlatform()
         {
             //Arrange
-            var testCommand = new Command
-            {
-                HowTo = "Do something awesome",
-                Platform = "xUnit",
-                CommandLine = "dotnet test"
-            };
+            //var testCommand = new Command
+            //{
+            //    HowTo = "Do something awesome",
+            //    Platform = "xUnit",
+            //    CommandLine = "dotnet test"
+            //};
+
             //Act
             testCommand.Platform = "xUnit";
             //Assert
@@ -41,12 +64,13 @@ namespace CommandAPI.Tests
         public void CanChangeCommandLine()
         {
             //Arrange
-            var testCommand = new Command
-            {
-                HowTo = "Do something awesome",
-                Platform = "xUnit",
-                CommandLine = "dotnet test"
-            };
+            //var testCommand = new Command
+            //{
+            //    HowTo = "Do something awesome",
+            //    Platform = "xUnit",
+            //    CommandLine = "dotnet test"
+            //};
+            
             //Act
             testCommand.CommandLine = "dotnet test";
             //Assert
